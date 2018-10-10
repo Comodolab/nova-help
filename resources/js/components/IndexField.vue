08c1f1d4fecf551679bbe5fdf8dcfe9042becf66
Alessandro Benoit <alessandro@comodolab.it>
2018-10-10 14:12:27 +0200

    On index view show the icon only when message is present

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="text-center">
         <div class="pr-4 comodolab-help-field-icon-container inline-block"
              :class="iconClass"
              :data-balloon="field.message.replace(/<(?:.|\n)*?>/gm, '')"
